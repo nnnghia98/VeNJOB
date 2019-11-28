d6b94eef9daa9a14484016301b32f2f359f4f6a8
nnnghia98 <nnnghia98@gmail.com>
2019-11-28 12:37:27 +0700

    header

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "static_pages#top_page"
   get "static_pages/top_page"
-  get "static_pages/favorite"
-  get "static_pages/history"
+  get "/favorite", to: "static_pages#favorite"
+  get "/history",to: "static_pages#history"
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,25 +1,21 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  def setup
-    @base_title = "VeNJOB"
-  end
-  
   test "should get top_page" do
-    get static_pages_top_page_url
+    get root_path
     assert_response :success
-    assert_select "title", "Top Page | #{@base_title}"
+    assert_select "title", "VeNJOB"
   end
 
   test "should get favorite" do
-    get static_pages_favorite_url
+    get favorite_url
     assert_response :success
-    assert_select "title", "Favorite | #{@base_title}"
+    assert_select "title", "Favorite | VeNJOB"
   end
 
   test "should get history" do
-    get static_pages_history_url
+    get history_url
     assert_response :success
-    assert_select "title", "History | #{@base_title}"
+    assert_select "title", "History | VeNJOB"
   end
 end
