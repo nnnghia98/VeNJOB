19ec4b1a4c473c8b892208171f673536de747156
nnnghia98 <nnnghia98@gmail.com>
2019-12-05 08:45:39 +0700

    edit migration file

--- db/migrate/20191202023059_add_company_code_to_company.rb
+++ db/migrate/20191202023059_add_company_code_to_company.rb
@@ -2,4 +2,5 @@ class AddCompanyCodeToCompany < ActiveRecord::Migration[6.0]
   def change
     add_column :companies, :company_code, :string, unique: true
   end
+  add_index :companies, :company_code, unique: true
 end
--- db/migrate/20191202023440_add_index_to_company_code.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToCompanyCode < ActiveRecord::Migration[6.0]
-  def change
-    add_index :companies, :company_code, unique: true
-  end
-end
