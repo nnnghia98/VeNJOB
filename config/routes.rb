e4c935d001483e3d76751dbe9aae4da177ca45da
nnnghia98 <nnnghia98@gmail.com>
2019-12-09 10:07:06 +0700

    send email request

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   resources :industries, only: :index
   resources :cities, only: :index
   resources :jobs, only: :index
-  devise_for :users
+  devise_for :users, controllers: {
+    # sessions: 'users/sessions'
+  }
   root "static_pages#index"
   get "static_pages/index"
   namespace :users do
