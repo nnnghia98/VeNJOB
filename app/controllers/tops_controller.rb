25a240327b5bbbf82658d4c1f8e1b977986b2353
nnnghia98 <nnnghia98@gmail.com>
2019-12-24 15:55:14 +0700

    move decorate to controller

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,6 +2,6 @@ class TopsController < ApplicationController
   def index
     @vn_cities = City.city_order
     @industries = Industry.industry_order
-    @latest_jobs = Job.latest_job
+    @latest_jobs = JobDecorator.decorate_collection(Job.latest_job)
   end
 end
