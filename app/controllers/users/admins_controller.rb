4c7dfb8cfb41996a7a87086747b6b594145e77ab
nnnghia98 <nnnghia98@gmail.com>
2020-03-03 10:42:28 +0700

    choose user's email

--- app/controllers/users/admins_controller.rb
+++ app/controllers/users/admins_controller.rb
@@ -4,7 +4,9 @@ class Users::AdminsController < ApplicationController
   def index
     redirect_to root_path unless current_user.role?
     params[:search_user] ||= ""
-    get_applied_jobs(params[:search_user])
+    @applied_jobs = get_applied_jobs(params[:search_user])
+    @user = User.find_by(email: params[:search_user])
+    @users = User.all
   end
 
   private
@@ -14,18 +16,20 @@ class Users::AdminsController < ApplicationController
   end
 
   def get_applied_jobs(email)
-    @applied_jobs = []
+    applied_jobs = []
     if email != ""
-      users = User.find_by(email: email)
-      redirect_to users_admin_path if users.nil?
+      user = User.find_by(email: email)
+      return redirect_to users_admin_path, notice: "User/job not found!" if user.blank?
 
-      @applied_jobs = users.jobs
+      applied_jobs = user.jobs
     elsif email = "" || nil
       all_applied_jobs = UserJob.where.not(applied_at: nil).to_a
 
       (0..all_applied_jobs.count - 1).each do |each_job|
-        @applied_jobs << (all_applied_jobs[each_job].job)
+        applied_jobs << (all_applied_jobs[each_job].job)
       end
+
+      applied_jobs
     end
   end
 end
--- app/helpers/job_helper.rb
+++ app/helpers/job_helper.rb
@@ -3,4 +3,8 @@ module JobHelper
     params[:city_id] ? "City: #{@jobs[0]["city"]}" :
                        (params[:industry_id] ? "Industry: #{@jobs[0]["industry"]}" : params[:search])
   end
+
+  def job_applied_at(job_id)
+    UserJob.where.not(applied_at: nil).find_by(job_id: job_id).applied_at
+  end
 end
